6ec74cb20a810bbd9f7240a4aee6fc24d335ca48
davidecaputo <daca9621@gmail.com>
2023-05-24 15:27:18 +0200

    Add cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,14 @@
 </div>
 <div class="bg-dark p-4">
     <div class="container py-5 d-flex flex-wrap gap-5">
-
+        @foreach ($comics as $comic)
+        <a href="#">
+            <div class="dc-card h-100">
+                <img src="{{ $comic['image'] }}" :alt="">
+                <span class="text-white">{{ $comic['title'] }}</span>
+            </div>
+        </a>
+        @endforeach
     </div>
 </div>
 <div class="bg-primary">
